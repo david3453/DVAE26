970375d80d5a04be4fa1291b92c926d52541ed12
daviostl101 <daviiddd999@gmail.com>
2025-01-18 14:33:31 +0100

    DoneFeatureEngineering

--- Data/FeatureEngineeringCode.py
+++ Data/FeatureEngineeringCode.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
+#This file was used to feature engineer the data using the sklearn and panda libraries. 
 # Load the CSV file into a DataFrame
 df = pd.read_csv('FeatureengineeredData2.0.csv')
 
-# Perform Label Encoding
-#label_encoder = LabelEncoder()
 
 # Specify the columns you want to convert
 columns_to_convert = ['gender_Female', 'gender_Male', 'gender_Other']
@@ -15,8 +14,6 @@ df[columns_to_convert] = df[columns_to_convert].astype(int)
 
 # Save the transformed DataFrame to a new CSV file
 df.to_csv('FeatureengineeredData3.0.csv', index=False)
-
-print("Conversion completed and saved to 'transformed_file.csv'")
 
 
 
