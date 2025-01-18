43fcbfc2165709ce5e8c994d66000dd4a93d9fb5
daviostl101 <daviiddd999@gmail.com>
2025-01-18 14:29:42 +0100

    Feature engineered data

--- Data/featEngi.py
+++ Data/featEngi.py
@@ -2,25 +2,21 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('Trdata1.0Feat.csv')
+df = pd.read_csv('FeatureengineeredData2.0.csv')
 
 # Perform Label Encoding
-label_encoder = LabelEncoder()
+#label_encoder = LabelEncoder()
 
-# Assume 'category_column' is the column to be label encoded
-df['gender'] = label_encoder.fit_transform(df['gender'])
-df['work_type'] = label_encoder.fit_transform(df['work_type'])
-df['smoking_status'] = label_encoder.fit_transform(df['smoking_status'])
+# Specify the columns you want to convert
+columns_to_convert = ['gender_Female', 'gender_Male', 'gender_Other']
 
-
-# Perform One-Hot Encoding
-# Assume 'another_category_column' is the column to be one-hot encoded
-df = pd.get_dummies(df, columns=['ever_married'])
-df = pd.get_dummies(df, columns=['Residence_type'])
+# Convert boolean values to integers
+df[columns_to_convert] = df[columns_to_convert].astype(int)
 
 # Save the transformed DataFrame to a new CSV file
-df.to_csv('FeaturengineeredData.csv', index=False)
+df.to_csv('FeatureengineeredData3.0.csv', index=False)
 
+print("Conversion completed and saved to 'transformed_file.csv'")
 
 
 
