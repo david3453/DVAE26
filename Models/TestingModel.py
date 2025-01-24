43923d177ac5d2f3565d62694fd34cf662ca1fea
daviostl101 <daviiddd999@gmail.com>
2025-01-24 21:17:44 +0100

    t

--- Models/TestingModel.py
+++ Models/TestingModel.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score, recall_score
+
 
 df = pd.read_csv(r'../Data/TrainingData2.0.csv')
 
@@ -22,7 +24,7 @@ y = df['stroke']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40)
 
 
-with open("automlF1Model.pkl", "rb") as f:
+with open("F1WeightedModel.pkl", "rb") as f:
     automl = pickle.load(f)
 pred = automl.predict(X_test)
 
@@ -30,14 +32,25 @@ pred = automl.predict(X_test)
 # Predict the labels for the test set
 y_pred = automl.predict(X_test)
 
+# Calculate precision
+precision = precision_score(y_test, y_pred)
+# Calculate recall
+recall = recall_score(y_test, y_pred)
+
+# Print the results
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+
 # Calculate the confusion matrix
+accuracy = accuracy_score(y_test, y_pred)
+f1_score = f1_score(y_test, y_pred)
+print(f"\nAccuracy: {accuracy}")
+print(f"F1 Score: {f1_score}\n")
+
 cm = confusion_matrix(y_test, y_pred)
 
 print("Confusion Matrix:")
 print(cm)
 
-accuracy = accuracy_score(y_test, y_pred)
-f1_score = f1_score(y_test, y_pred)
-print(f"\n\nAccuracy: {accuracy}\n")
-print(f"F1 Score: {f1_score}\n")
+
 
